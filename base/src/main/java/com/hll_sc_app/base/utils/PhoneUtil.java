7b0d3d26777fdc872412ed3e253ae7d4226b0bbf
xuezhixin <xuezhixin@hualala.com>
2019-07-08 11:27:17 +0800

    修复错误格式手机号未正确显示

--- base/src/main/java/com/hll_sc_app/base/utils/PhoneUtil.java
+++ base/src/main/java/com/hll_sc_app/base/utils/PhoneUtil.java
@@ -2,6 +2,7 @@ package com.hll_sc_app.base.utils;
 
 import android.os.Build;
 import android.telephony.PhoneNumberUtils;
+import android.text.TextUtils;
 
 import java.util.Locale;
 
@@ -12,10 +13,10 @@ public class PhoneUtil {
      * @param phoneNum 手机号
      */
     public static String formatPhoneNum(String phoneNum) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return PhoneNumberUtils.formatNumber(phoneNum, Locale.CHINA.getCountry());
-        } else {
-            return PhoneNumberUtils.formatNumber(phoneNum);
-        }
+        String result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            result = PhoneNumberUtils.formatNumber(phoneNum, Locale.CHINA.getCountry());
+        else result = PhoneNumberUtils.formatNumber(phoneNum);
+        return TextUtils.isEmpty(result) ? phoneNum : result;
     }
 }
