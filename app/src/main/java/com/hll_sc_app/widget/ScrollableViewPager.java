9d94cda620f9f73346f3cd2d58647fedc0a51b3f
xuezhixin <xuezhixin@hualala.com>
2019-07-31 12:16:35 +0800

    调整控件命名

---
+++ app/src/main/java/com/hll_sc_app/widget/ScrollableViewPager.java
@@ -0,0 +1,36 @@
+package com.hll_sc_app.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class ScrollableViewPager extends ViewPager {
+    private boolean isScrollable = false;
+
+    public ScrollableViewPager(Context context) {
+        this(context, null);
+    }
+
+    public ScrollableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (isScrollable)
+            return super.onInterceptTouchEvent(ev);
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (isScrollable)
+            return super.onTouchEvent(ev);
+        return true;
+    }
+
+    public void setScrollable(boolean scrollable) {
+        isScrollable = scrollable;
+    }
+}
