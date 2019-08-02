23789b500b2074ce28cd4be6877c3410a27c81b9
zhuyingsong <zhuyingsong@hualala.com>
2019-08-02 14:51:09 +0800

    代仓管理-我是货主界面

--- app/src/main/java/com/hll_sc_app/app/mine/MineHomeFragment.java
+++ app/src/main/java/com/hll_sc_app/app/mine/MineHomeFragment.java
@@ -176,8 +176,8 @@ public class MineHomeFragment extends BaseLoadFragment implements MineHomeFragme
     }
 
     @OnClick({R.id.txt_wallet, R.id.txt_agreement_price, R.id.txt_warehouse_manage, R.id.txt_cooperation_purchaser,
-            R.id.txt_return_audit, R.id.img_setting, R.id.txt_price_setting, R.id.txt_report_center,
-            R.id.txt_staff_manage, R.id.txt_delivery_manage})
+        R.id.txt_return_audit, R.id.img_setting, R.id.txt_price_setting, R.id.txt_report_center,
+        R.id.txt_staff_manage, R.id.txt_delivery_manage})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.txt_wallet:
@@ -195,6 +195,7 @@ public class MineHomeFragment extends BaseLoadFragment implements MineHomeFragme
                 RouterUtil.goToActivity(RouterConfig.DELIVERY_MANAGE);
                 break;
             case R.id.txt_warehouse_manage:
+                RouterUtil.goToActivity(RouterConfig.WAREHOUSE_INTRODUCE);
                 break;
             case R.id.txt_cooperation_purchaser:
                 RouterUtil.goToActivity(RouterConfig.COOPERATION_PURCHASER_LIST);
--- base/src/main/java/com/hll_sc_app/base/utils/router/RouterConfig.java
+++ base/src/main/java/com/hll_sc_app/base/utils/router/RouterConfig.java
@@ -28,6 +28,10 @@ public class RouterConfig {
      */
     public static final String DELIVERY_MANAGE = "/activity/deliveryManage";
     /**
+     * 代仓管理-我是货主默认介绍页面
+     */
+    public static final String WAREHOUSE_INTRODUCE = "/activity/warehouse/introduce";
+    /**
      * 配送管理-配送时效管理
      */
     public static final String DELIVERY_AGEING_MANAGE = "/activity/deliveryManage/ageing";
