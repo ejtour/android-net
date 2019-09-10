2a8e80911d60a57838f7ef1e17ef6ca0435a9827
zhouce <zhouce@hualala.com>
2019-09-10 15:53:16 +0800

    fix 库存管理的样式

--- app/src/main/java/com/hll_sc_app/app/stockmanage/stockquery/StockQueryActivity.java
+++ app/src/main/java/com/hll_sc_app/app/stockmanage/stockquery/StockQueryActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -35,6 +36,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 @Route(path = RouterConfig.ACTIVITY_STOCK_QUERY_LIST)
@@ -153,6 +155,17 @@ public class StockQueryActivity extends BaseLoadActivity implements IStockQueryC
     public void hideLoading() {
         super.hideLoading();
         mRefresh.closeHeaderOrFooter();
+    }
+
+    @OnClick({R.id.img_close})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_close:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 
     private class GoodAdapter extends BaseQuickAdapter<GoodsBean, BaseViewHolder> {
