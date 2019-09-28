7a9f539ac7ddefc6453341221af4c0a5c8e83027
xuezhixin <xuezhixin@hualala.com>
2019-09-28 15:31:11 +0800

    add 配送路线列表单位、跳转

--- app/src/main/java/com/hll_sc_app/app/deliveryroute/DeliveryRouteAdapter.java
+++ app/src/main/java/com/hll_sc_app/app/deliveryroute/DeliveryRouteAdapter.java
@@ -1,8 +1,11 @@
 package com.hll_sc_app.app.deliveryroute;
 
+import android.view.ViewGroup;
+
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.hll_sc_app.R;
+import com.hll_sc_app.base.utils.UIUtils;
 import com.hll_sc_app.bean.other.RouteBean;
 import com.hll_sc_app.citymall.util.CommonUtils;
 
@@ -14,17 +17,29 @@ import com.hll_sc_app.citymall.util.CommonUtils;
 public class DeliveryRouteAdapter extends BaseQuickAdapter<RouteBean, BaseViewHolder> {
     DeliveryRouteAdapter() {
         super(R.layout.item_delivery_route);
+        setOnItemChildClickListener((adapter, view, position) -> {
+            if (view.getTag() != null)
+                UIUtils.callPhone(view.getContext(), view.getTag().toString());
+        });
+    }
+
+    @Override
+    protected BaseViewHolder onCreateDefViewHolder(ViewGroup parent, int viewType) {
+        BaseViewHolder helper = super.onCreateDefViewHolder(parent, viewType);
+        helper.addOnClickListener(R.id.idr_driver);
+        return helper;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, RouteBean item) {
         helper.setText(R.id.idr_name, String.format("%s（%s）", item.getLineName(), item.getLineCode()))
-                .setText(R.id.idr_shop_num, "配送门店：" + CommonUtils.formatNum(item.getDemandNum()))
-                .setText(R.id.idr_driver, "司机：" + item.getDriverName())
-                .setText(R.id.idr_plate_number, "车牌：" + item.getPlateNumber())
-                .setText(R.id.idr_done, "已完成：" + CommonUtils.formatNum(item.getCompletedBillNum()))
-                .setText(R.id.idr_no_delivery, "未配送：" + CommonUtils.formatNum(item.getNoDeliveryBillNum()))
-                .setText(R.id.idr_on_time, "准时：" + CommonUtils.formatNum(item.getOnTimeBillNum()))
-                .setText(R.id.idr_delay, "延迟：" + CommonUtils.formatNum(item.getDelayBillNum()));
+                .setText(R.id.idr_shop_num, String.format("配送门店：%s", CommonUtils.formatNum(item.getDemandNum())))
+                .setText(R.id.idr_driver, String.format("司机：%s", item.getDriverName()))
+                .setTag(R.id.idr_driver, item.getMobilePhone())
+                .setText(R.id.idr_plate_number, String.format("车牌：%s", item.getPlateNumber()))
+                .setText(R.id.idr_done, String.format("已完成：%s单", CommonUtils.formatNum(item.getCompletedBillNum())))
+                .setText(R.id.idr_no_delivery, String.format("未配送：%s单", CommonUtils.formatNum(item.getNoDeliveryBillNum())))
+                .setText(R.id.idr_on_time, String.format("准时：%s单", CommonUtils.formatNum(item.getOnTimeBillNum())))
+                .setText(R.id.idr_delay, String.format("延迟：%s单", CommonUtils.formatNum(item.getDelayBillNum())));
     }
 }
