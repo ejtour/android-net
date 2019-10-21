2215ba07be062a7bf68518c57145ceabaf231688
xuezhixin <xuezhixin@hualala.com>
2019-10-21 20:21:46 +0800

    add 新品反馈搜索意向客户接口

---
+++ app/src/main/java/com/hll_sc_app/bean/common/IntentionCustomerBean.java
@@ -0,0 +1,208 @@
+package com.hll_sc_app.bean.common;
+
+/**
+ * @author <a href="mailto:xuezhixin@hualala.com">Vixb</a>
+ * @since 2019/10/21
+ */
+
+public class IntentionCustomerBean {
+
+    private String customerAddress;
+    private String customerDistrictCode;
+    private String customerProvince;
+    private String groupID;
+    private String employeeID;
+    private String customerCity;
+    private String customerLevel;
+    private String customerName;
+    private String customerLinkman;
+    private int customerStatus;
+    private String customerDistrict;
+    private String customerPhone;
+    private int customerType;
+    private String visitTime;
+    private String createTime;
+    private String customerCityCode;
+    private int shopCount;
+    private int action;
+    private String customerProvinceCode;
+    private int customerSource;
+    private String id;
+    private String customerAdmin;
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getCustomerDistrictCode() {
+        return customerDistrictCode;
+    }
+
+    public void setCustomerDistrictCode(String customerDistrictCode) {
+        this.customerDistrictCode = customerDistrictCode;
+    }
+
+    public String getCustomerProvince() {
+        return customerProvince;
+    }
+
+    public void setCustomerProvince(String customerProvince) {
+        this.customerProvince = customerProvince;
+    }
+
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getCustomerCity() {
+        return customerCity;
+    }
+
+    public void setCustomerCity(String customerCity) {
+        this.customerCity = customerCity;
+    }
+
+    public String getCustomerLevel() {
+        return customerLevel;
+    }
+
+    public void setCustomerLevel(String customerLevel) {
+        this.customerLevel = customerLevel;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerLinkman() {
+        return customerLinkman;
+    }
+
+    public void setCustomerLinkman(String customerLinkman) {
+        this.customerLinkman = customerLinkman;
+    }
+
+    public int getCustomerStatus() {
+        return customerStatus;
+    }
+
+    public void setCustomerStatus(int customerStatus) {
+        this.customerStatus = customerStatus;
+    }
+
+    public String getCustomerDistrict() {
+        return customerDistrict;
+    }
+
+    public void setCustomerDistrict(String customerDistrict) {
+        this.customerDistrict = customerDistrict;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public int getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(int customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getVisitTime() {
+        return visitTime;
+    }
+
+    public void setVisitTime(String visitTime) {
+        this.visitTime = visitTime;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCustomerCityCode() {
+        return customerCityCode;
+    }
+
+    public void setCustomerCityCode(String customerCityCode) {
+        this.customerCityCode = customerCityCode;
+    }
+
+    public int getShopCount() {
+        return shopCount;
+    }
+
+    public void setShopCount(int shopCount) {
+        this.shopCount = shopCount;
+    }
+
+    public int getAction() {
+        return action;
+    }
+
+    public void setAction(int action) {
+        this.action = action;
+    }
+
+    public String getCustomerProvinceCode() {
+        return customerProvinceCode;
+    }
+
+    public void setCustomerProvinceCode(String customerProvinceCode) {
+        this.customerProvinceCode = customerProvinceCode;
+    }
+
+    public int getCustomerSource() {
+        return customerSource;
+    }
+
+    public void setCustomerSource(int customerSource) {
+        this.customerSource = customerSource;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCustomerAdmin() {
+        return customerAdmin;
+    }
+
+    public void setCustomerAdmin(String customerAdmin) {
+        this.customerAdmin = customerAdmin;
+    }
+}
