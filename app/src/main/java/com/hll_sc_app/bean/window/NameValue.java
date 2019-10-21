e2a8d3c1ac7ca21f58fde613e27d5d94c5da7af0
xuezhixin <xuezhixin@hualala.com>
2019-10-21 20:07:50 +0800

    update NameValue类序列化

--- app/src/main/java/com/hll_sc_app/bean/window/NameValue.java
+++ app/src/main/java/com/hll_sc_app/bean/window/NameValue.java
@@ -1,17 +1,47 @@
 package com.hll_sc_app.bean.window;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * @author <a href="mailto:xuezhixin@hualala.com">Vixb</a>
  * @since 2019/6/17
  */
 
-public class NameValue {
+public class NameValue implements Parcelable {
+    public static final Creator<NameValue> CREATOR = new Creator<NameValue>() {
+        @Override
+        public NameValue createFromParcel(Parcel in) {
+            return new NameValue(in);
+        }
+
+        @Override
+        public NameValue[] newArray(int size) {
+            return new NameValue[size];
+        }
+    };
     private String name;
     private String value;
 
     public NameValue(String name, String value) {
         this.name = name;
         this.value = value;
+    }
+
+    protected NameValue(Parcel in) {
+        name = in.readString();
+        value = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(value);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public String getName() {
