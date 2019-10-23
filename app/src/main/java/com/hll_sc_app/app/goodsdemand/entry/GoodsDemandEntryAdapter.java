779f0cd6e5d420d6c89e871500635748aae05eba
xuezhixin <xuezhixin@hualala.com>
2019-10-23 19:06:02 +0800

    update 帮助方法提取

--- app/src/main/java/com/hll_sc_app/app/goodsdemand/entry/GoodsDemandEntryAdapter.java
+++ app/src/main/java/com/hll_sc_app/app/goodsdemand/entry/GoodsDemandEntryAdapter.java
@@ -6,6 +6,8 @@ import android.widget.TextView;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.hll_sc_app.R;
+import com.hll_sc_app.app.goodsdemand.GoodsDemandHelper;
+import com.hll_sc_app.app.goodsdemand.detail.GoodsDemandDetailActivity;
 import com.hll_sc_app.bean.goodsdemand.GoodsDemandBean;
 
 /**
@@ -22,32 +24,10 @@ public class GoodsDemandEntryAdapter extends BaseQuickAdapter<GoodsDemandBean, B
     protected void convert(BaseViewHolder helper, GoodsDemandBean item) {
         ((TextView) helper.setText(R.id.gde_goods_name, item.getProductName())
                 .setText(R.id.gde_name, item.getSupplyName())
-                .setText(R.id.gde_status, getStatus(item.getStatus()))
+                .setText(R.id.gde_status, GoodsDemandHelper.getStatus(item.getStatus()))
                 .setTextColor(R.id.gde_status, ContextCompat.getColor(helper.itemView.getContext(),
                         item.getStatus() == 3 ? R.color.colorPrimary : R.color.color_f6bb42))
                 .getView(R.id.gde_status))
-                .setCompoundDrawablesWithIntrinsicBounds(getIcon(item.getStatus()), 0, 0, 0);
-    }
-
-    private String getStatus(int status) {
-        switch (status) {
-            case 1:
-                return "待回复";
-            case 2:
-                return "已回复";
-            default:
-                return "已上架";
-        }
-    }
-
-    private int getIcon(int status) {
-        switch (status) {
-            case 1:
-                return R.drawable.ic_warn;
-            case 2:
-                return R.drawable.ic_yellow_ok;
-            default:
-                return R.drawable.ic_blue_ok;
-        }
+                .setCompoundDrawablesWithIntrinsicBounds(GoodsDemandHelper.getIcon(item.getStatus()), 0, 0, 0);
     }
 }
