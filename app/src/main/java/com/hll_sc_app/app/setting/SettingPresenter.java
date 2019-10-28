aced90f54a9fba45895284d46845fc9afe95f8bf
xuezhixin <xuezhixin@hualala.com>
2019-10-28 10:32:04 +0800

    add 清除缓存

--- app/src/main/java/com/hll_sc_app/app/setting/SettingPresenter.java
+++ app/src/main/java/com/hll_sc_app/app/setting/SettingPresenter.java
@@ -1,16 +1,18 @@
 package com.hll_sc_app.app.setting;
 
-import com.hll_sc_app.api.UserService;
-import com.hll_sc_app.base.UseCaseException;
-import com.hll_sc_app.base.bean.BaseMapReq;
-import com.hll_sc_app.base.http.ApiScheduler;
-import com.hll_sc_app.base.http.BaseCallback;
-import com.hll_sc_app.base.http.Precondition;
-import com.hll_sc_app.base.utils.UserConfig;
+import android.content.Context;
+
+import com.hll_sc_app.base.http.SimpleObserver;
+import com.hll_sc_app.base.utils.glide.GlideApp;
 import com.hll_sc_app.citymall.util.CommonUtils;
+import com.hll_sc_app.rest.User;
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 import static com.uber.autodispose.AutoDispose.autoDisposable;
 
@@ -20,48 +22,38 @@ import static com.uber.autodispose.AutoDispose.autoDisposable;
  * @author zhuyingsong
  * @date 2019/6/19
  */
-public class SettingPresenter implements SettingContract.ISaleUnitNameAddPresenter {
-    private SettingContract.ISaleUnitNameAddView mView;
+public class SettingPresenter implements SettingContract.ISettingPresenter {
+    private SettingContract.ISettingView mView;
 
     static SettingPresenter newInstance() {
         return new SettingPresenter();
     }
 
     @Override
-    public void register(SettingContract.ISaleUnitNameAddView view) {
+    public void register(SettingContract.ISettingView view) {
         this.mView = CommonUtils.checkNotNull(view);
     }
 
     @Override
     public void logout() {
-        BaseMapReq req = BaseMapReq.newBuilder()
-            .put("accessToken", UserConfig.accessToken())
-            .create();
-        UserService.INSTANCE.logout(req)
-            .compose(ApiScheduler.getObservableScheduler())
-            .map(new Precondition<>())
-            .doOnSubscribe(disposable -> mView.showLoading())
-            .doFinally(() -> mView.hideLoading())
-            .as(autoDisposable(AndroidLifecycleScopeProvider.from(mView.getOwner())))
-            .subscribe(new BaseCallback<Object>() {
-                @Override
-                public void onSuccess(Object resp) {
-                    mView.logoutSuccess();
-                }
-
-                @Override
-                public void onFailure(UseCaseException e) {
-                }
-            });
+        User.logout(new SimpleObserver<Object>(mView) {
+            @Override
+            public void onSuccess(Object o) {
+                mView.logoutSuccess();
+            }
+        });
     }
 
-    public static Observable<Object> logoutObservable() {
-        BaseMapReq req = BaseMapReq.newBuilder()
-                .put("accessToken", UserConfig.accessToken())
-                .create();
-        return UserService.INSTANCE
-                .logout(req)
-                .compose(ApiScheduler.getObservableScheduler())
-                .map(new Precondition<>());
+    @Override
+    public void cleanCache() {
+        Observable.timer(0, TimeUnit.MILLISECONDS, Schedulers.io())
+                .map(aLong -> {
+                    GlideApp.get((Context) mView).clearDiskCache();
+                    return aLong;
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> mView.startClean())
+                .as(autoDisposable(AndroidLifecycleScopeProvider.from(mView.getOwner())))
+                .subscribe(o -> mView.cleanSuccess());
     }
 }
---
+++ app/src/main/java/com/hll_sc_app/rest/User.java
@@ -0,0 +1,28 @@
+package com.hll_sc_app.rest;
+
+import com.hll_sc_app.api.UserService;
+import com.hll_sc_app.base.bean.BaseMapReq;
+import com.hll_sc_app.base.http.ApiScheduler;
+import com.hll_sc_app.base.http.SimpleObserver;
+import com.hll_sc_app.base.utils.UserConfig;
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
+
+import static com.uber.autodispose.AutoDispose.autoDisposable;
+
+/**
+ * @author <a href="mailto:xuezhixin@hualala.com">Vixb</a>
+ * @since 2019/10/28
+ */
+
+public class User {
+
+    public static void logout(SimpleObserver<Object> observer) {
+        UserService.INSTANCE
+                .logout(BaseMapReq.newBuilder()
+                        .put("accessToken", UserConfig.accessToken())
+                        .create())
+                .compose(ApiScheduler.getDefaultObservableWithLoadingScheduler(observer))
+                .as(autoDisposable(AndroidLifecycleScopeProvider.from(observer.getOwner())))
+                .subscribe(observer);
+    }
+}
