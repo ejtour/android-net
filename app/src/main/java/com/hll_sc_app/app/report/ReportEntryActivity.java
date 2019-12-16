c33d3f5d18b07c8665ecad1061d446204eb4cbd9
xuezhixin <xuezhixin@hualala.com>
2019-12-16 10:44:58 +0800

    add crm端门店销售汇总表

--- app/src/main/java/com/hll_sc_app/app/report/ReportEntryActivity.java
+++ app/src/main/java/com/hll_sc_app/app/report/ReportEntryActivity.java
@@ -54,6 +54,8 @@ public class ReportEntryActivity extends BaseLoadActivity {
             list.add(new ReportItem(R.drawable.ic_report_shop_loss, "流失门店明细表", RouterConfig.REPORT_SHOP_LOSS_DETAIL, true));
 
             list.add(new ReportItem(R.drawable.ic_report_group_loss, "日报统计", RouterConfig.REPORT_SALES_DAY_REPORT, true));
+
+            list.add(new ReportItem(R.drawable.ic_report_shop_sales, "门店销售汇总表", RouterConfig.CUSTOMER_SALE_AGGREGATION_DETAIL, true));
         } else {
             list.add(new ReportItem(R.drawable.ic_report_sales_statistics, "商品销量统计汇总", RouterConfig.REPORT_PRODUCT_SALES_STATISTICS));
             list.add(new ReportItem(R.drawable.ic_report_order_goods_details, "客户订货统计", RouterConfig.REPORT_ORDER_GOODS, true));
