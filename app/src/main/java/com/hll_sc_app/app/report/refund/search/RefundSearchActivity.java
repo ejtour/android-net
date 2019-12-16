d621537f8e3e906152bf23a1e31f0a3892396ee0
xuezhixin <xuezhixin@hualala.com>
2019-12-16 19:40:37 +0800

    update 退货客户搜索

--- app/src/main/java/com/hll_sc_app/app/report/refund/search/RefundSearchActivity.java
+++ app/src/main/java/com/hll_sc_app/app/report/refund/search/RefundSearchActivity.java
@@ -6,41 +6,33 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.text.TextUtils;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.ImageView;
+
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.flyco.tablayout.SlidingTabLayout;
 import com.githang.statusbar.StatusBarCompat;
 import com.hll_sc_app.R;
-import com.hll_sc_app.app.goods.add.specs.GoodsSpecsAddActivity;
 import com.hll_sc_app.base.BaseLoadActivity;
 import com.hll_sc_app.base.utils.router.RouterConfig;
-import com.hll_sc_app.base.utils.router.RouterUtil;
-import com.hll_sc_app.citymall.util.ViewUtils;
+import com.hll_sc_app.widget.SearchTitleBar;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 @Route(path = RouterConfig.REPORT_REFUNDED_SEARCH)
 public class RefundSearchActivity extends BaseLoadActivity {
 
     public static final int INT_PURCHASER = 1;
-    @BindView(R.id.edt_search)
-    EditText mEdtSearch;
-    @BindView(R.id.img_clear)
-    ImageView mImgClear;
-    @BindView(R.id.tl_title)
-    SlidingTabLayout mTlTitle;
-    @BindView(R.id.viewPager)
+    @BindView(R.id.rrs_title_bar)
+    SearchTitleBar mTitleBar;
+    @BindView(R.id.rrs_tab_layout)
+    SlidingTabLayout mTabLayout;
+    @BindView(R.id.rrs_view_pager)
     ViewPager mViewPager;
     @Autowired(name = "object0")
     int mTag;
@@ -58,86 +50,40 @@ public class RefundSearchActivity extends BaseLoadActivity {
 
 
     public void initView(){
-        mEdtSearch.addTextChangedListener((GoodsSpecsAddActivity.CheckTextWatcher) s
-                -> mImgClear.setVisibility(TextUtils.isEmpty(s) ? View.GONE : View.VISIBLE));
-        mEdtSearch.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                toSearch();
-            }
-            return true;
-        });
-        ArrayList<BaseRefundSearchFragment> list = new ArrayList<>(2);
+        mTitleBar.setOnSearchListener(this::toSearch);
+        ArrayList<RefundedSearchFragment> list = new ArrayList<>(2);
         //0-搜索集团 1-搜索门店
         list.add(RefundedSearchFragment.newInstance("0"));
         list.add(RefundedSearchFragment.newInstance("1"));
         mAdapter = new RefundSearchActivity.PagerAdapter(getSupportFragmentManager(), list);
         mViewPager.setAdapter(mAdapter);
-        mTlTitle.setViewPager(mViewPager, new String[]{"采购商集团", "采购商门店"});
+        mTabLayout.setViewPager(mViewPager, new String[]{"采购商集团", "采购商门店"});
         if (mTag == INT_PURCHASER) {
             mViewPager.setCurrentItem(1);
         }
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                // no-op
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                if (position == 0) {
-                    mEdtSearch.setHint("搜索");
-                } else {
-                    mEdtSearch.setHint("搜索");
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                // no-op
-            }
-        });
     }
 
     private void toSearch() {
-        ViewUtils.clearEditFocus(mEdtSearch);
-        BaseRefundSearchFragment fragment = mAdapter.getItem(mViewPager.getCurrentItem());
+        RefundedSearchFragment fragment = mAdapter.getItem(mViewPager.getCurrentItem());
         if (fragment != null) {
             fragment.toSearch();
         }
     }
 
     public String getSearchText() {
-        return mEdtSearch.getText().toString().trim();
-    }
-
-    @OnClick({R.id.img_back, R.id.img_clear, R.id.txt_search})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.img_back:
-                finish();
-                break;
-            case R.id.img_clear:
-                mEdtSearch.setText("");
-                toSearch();
-                break;
-            case R.id.txt_search:
-                toSearch();
-                break;
-            default:
-                break;
-        }
+        return mTitleBar.getSearchContent();
     }
 
     static class PagerAdapter extends FragmentPagerAdapter {
-        private List<BaseRefundSearchFragment> mFragmentList;
+        private List<RefundedSearchFragment> mFragmentList;
 
-        public PagerAdapter(FragmentManager fm, List<BaseRefundSearchFragment> fragmentList) {
+        public PagerAdapter(FragmentManager fm, List<RefundedSearchFragment> fragmentList) {
             super(fm);
             this.mFragmentList = fragmentList;
         }
 
         @Override
-        public BaseRefundSearchFragment getItem(int position) {
+        public RefundedSearchFragment getItem(int position) {
             return mFragmentList.get(position);
         }
 
--- app/src/main/java/com/hll_sc_app/app/report/refund/search/RefundedSearchFragment.java
+++ app/src/main/java/com/hll_sc_app/app/report/refund/search/RefundedSearchFragment.java
@@ -1,26 +1,33 @@
 package com.hll_sc_app.app.report.refund.search;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.sdk.android.ams.common.util.StringUtil;
 import com.hll_sc_app.R;
+import com.hll_sc_app.base.BaseLazyFragment;
 import com.hll_sc_app.base.UseCaseException;
+import com.hll_sc_app.base.utils.UIUtils;
 import com.hll_sc_app.base.utils.router.RouterConfig;
 import com.hll_sc_app.bean.report.search.SearchReq;
 import com.hll_sc_app.bean.report.search.SearchResultItem;
+import com.hll_sc_app.widget.SimpleDecoration;
+
 import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 @Route(path = RouterConfig.REPORT_REFUND_SEARCH_FRAGMENT)
-public class RefundedSearchFragment extends BaseRefundSearchFragment implements RefundSearchFragmentContract.IRefundSearchView {
+public class RefundedSearchFragment extends BaseLazyFragment implements RefundSearchFragmentContract.IRefundSearchView {
 
     private final static  String SEARCH_TYPE = "search_type";
     private RefundSearchFragmentContract.IRefundSearchPresenter mPresenter;
@@ -50,6 +57,14 @@ public class RefundedSearchFragment extends BaseRefundSearchFragment implements 
         //0-供应商 1-采购商
         searchReq.setSource(0);
         return searchReq;
+    }
+
+    private String getSearchParam() {
+        String searchPram = "";
+        if (getActivity() instanceof RefundSearchActivity) {
+            searchPram = ((RefundSearchActivity) getActivity()).getSearchText();
+        }
+        return searchPram;
     }
 
     @Override
@@ -82,6 +97,9 @@ public class RefundedSearchFragment extends BaseRefundSearchFragment implements 
     private void initView() {
         mAdapter = new RefundSearchAdapter();
         recyclerView.setAdapter(mAdapter);
+        SimpleDecoration decor = new SimpleDecoration(ContextCompat.getColor(requireActivity(), R.color.color_eeeeee), UIUtils.dip2px(1));
+        decor.setLineMargin(UIUtils.dip2px(10), 0, UIUtils.dip2px(10), 0, Color.WHITE);
+        recyclerView.addItemDecoration(decor);
         mAdapter.setOnItemClickListener((adapter, view, position) -> {
             SearchResultItem item = (SearchResultItem) adapter.getItem(position);
             Intent intent = new Intent();
@@ -97,7 +115,6 @@ public class RefundedSearchFragment extends BaseRefundSearchFragment implements 
         toSearch();
     }
 
-    @Override
     public void toSearch() {
         searchReq.setSearchWords(getSearchParam());
         searchReq.setType(Integer.valueOf(searchType));
