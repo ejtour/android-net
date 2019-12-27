3d9583519785a61b88b0db206f6916ff2f85fb62
zhouce <zhouce@hualala.com>
2019-12-27 15:22:41 +0800

    add 合同管理开发中

--- app/src/main/java/com/hll_sc_app/app/contractmanage/ContractManagePresent.java
+++ app/src/main/java/com/hll_sc_app/app/contractmanage/ContractManagePresent.java
@@ -1,7 +1,23 @@
 package com.hll_sc_app.app.contractmanage;
 
+import com.hll_sc_app.api.CooperationPurchaserService;
+import com.hll_sc_app.base.UseCaseException;
+import com.hll_sc_app.base.bean.BaseMapReq;
+import com.hll_sc_app.base.bean.UserBean;
+import com.hll_sc_app.base.greendao.GreenDaoUtils;
+import com.hll_sc_app.base.http.ApiScheduler;
+import com.hll_sc_app.base.http.BaseCallback;
+import com.hll_sc_app.base.http.Precondition;
+import com.hll_sc_app.base.utils.UserConfig;
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
+
+import static com.uber.autodispose.AutoDispose.autoDisposable;
+
 public class ContractManagePresent implements IContractManageContract.IPresent {
     private IContractManageContract.IView mView;
+    private final int PAGE_SIZE =20;
+    private int pageNum = 1;
+    private int pageTempNum = 1;
 
     public static ContractManagePresent newInstance() {
         return new ContractManagePresent();
@@ -10,5 +26,55 @@ public class ContractManagePresent implements IContractManageContract.IPresent {
     @Override
     public void register(IContractManageContract.IView view) {
         mView = view;
+    }
+
+
+    @Override
+    public void queryList(boolean isLoading) {
+        UserBean userBean= GreenDaoUtils.getUser();
+        if (userBean==null){
+            return;
+        }
+        BaseMapReq req = BaseMapReq.newBuilder()
+                .put("contractCode", mView.getContractCode())
+                .put("contractName", mView.getContractName())
+                .put("purchaserName", mView.getPurchaserName())
+                .put("groupID",userBean.getGroupID())
+                .put("isCloseToExpiration",mView.getDays())
+                .put("pageNum", String.valueOf(pageTempNum))
+                .put("pageSize", String.valueOf(PAGE_SIZE))
+
+                .put("originator", "1")
+                .put("originator", "1")
+
+                .create();
+        CooperationPurchaserService.INSTANCE.delCooperationPurchaser(req)
+                .compose(ApiScheduler.getObservableScheduler())
+                .map(new Precondition<>())
+                .doOnSubscribe(disposable -> mView.showLoading())
+                .doFinally(() -> mView.hideLoading())
+                .as(autoDisposable(AndroidLifecycleScopeProvider.from(mView.getOwner())))
+                .subscribe(new BaseCallback<Object>() {
+                    @Override
+                    public void onSuccess(Object resp) {
+                        mView.showToast("解除关系成功");
+                        queryPurchaserList(true);
+                    }
+
+                    @Override
+                    public void onFailure(UseCaseException e) {
+                        mView.showError(e);
+                    }
+                });
+    }
+
+    @Override
+    public void queryMore() {
+
+    }
+
+    @Override
+    public void refresh() {
+
     }
 }
--- app/src/main/java/com/hll_sc_app/app/contractmanage/IContractManageContract.java
+++ app/src/main/java/com/hll_sc_app/app/contractmanage/IContractManageContract.java
@@ -2,13 +2,34 @@ package com.hll_sc_app.app.contractmanage;
 
 import com.hll_sc_app.base.ILoadView;
 import com.hll_sc_app.base.IPresenter;
+import com.hll_sc_app.bean.contract.ContractListResp;
 
 public interface IContractManageContract {
     interface IView extends ILoadView {
 
+        void querySuccess(ContractListResp resp, boolean isMore);
+
+        String getSpanTimeStart();
+        String getSpanTimeEnd();
+
+        String getStatus();
+
+        String getDays();
+
+        String getContractCode();
+
+        String getContractName();
+
+        String getPurchaserName();
+
     }
 
     interface IPresent extends IPresenter<IView> {
+        void queryList(boolean isLoading);
+
+        void queryMore();
+
+        void refresh();
 
     }
 }
