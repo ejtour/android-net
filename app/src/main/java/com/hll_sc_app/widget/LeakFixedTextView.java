12f9e6b3bb849fdfc9a7537faaa63e1077228259
xuezhixin <xuezhixin@hualala.com>
2020-04-17 11:30:37 +0800

    fix LeakFixedTextView 状态错误

--- app/src/main/java/com/hll_sc_app/widget/LeakFixedTextView.java
+++ app/src/main/java/com/hll_sc_app/widget/LeakFixedTextView.java
@@ -11,6 +11,8 @@ import android.util.AttributeSet;
  */
 
 public class LeakFixedTextView extends AppCompatTextView {
+    private boolean mSingleLine;
+
     public LeakFixedTextView(Context context) {
         this(context, null);
     }
@@ -21,13 +23,25 @@ public class LeakFixedTextView extends AppCompatTextView {
 
     public LeakFixedTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (getMovementMethod() == null && !mSingleLine) {
+            setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
+
+    @Override
+    public void setSingleLine(boolean singleLine) {
+        mSingleLine = singleLine;
+        super.setSingleLine(singleLine);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         setMovementMethod(null);
-        setText("");
         super.onDetachedFromWindow();
     }
 }
