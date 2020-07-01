f92fd79b8c2ec747525ba6a1bb77d95b7f0c633d
xuezhixin <xuezhixin@hualala.com>
2020-07-01 10:15:10 +0800

    update 意见反馈代码优化

--- app/src/main/java/com/hll_sc_app/app/feedbackcomplain/feedback/add/FeedbackAddActivity.java
+++ app/src/main/java/com/hll_sc_app/app/feedbackcomplain/feedback/add/FeedbackAddActivity.java
@@ -1,14 +1,10 @@
 package com.hll_sc_app.app.feedbackcomplain.feedback.add;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
@@ -20,30 +16,28 @@ import com.hll_sc_app.base.BaseLoadActivity;
 import com.hll_sc_app.base.utils.router.LoginInterceptor;
 import com.hll_sc_app.base.utils.router.RouterConfig;
 import com.hll_sc_app.base.utils.router.RouterUtil;
-import com.hll_sc_app.base.widget.ImgUploadBlock;
 import com.hll_sc_app.bean.complain.FeedbackDetailResp;
-import com.hll_sc_app.widget.IUploadImageMethod;
+import com.hll_sc_app.widget.ImageUploadGroup;
 import com.hll_sc_app.widget.TitleBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 import butterknife.Unbinder;
 
 /**
  * 意见反馈新增
  */
 @Route(path = RouterConfig.ACTIVITY_FEED_BACK_ADD)
-public class FeedbackAddActivity extends BaseLoadActivity implements IFeedbackAddContract.IView, IUploadImageMethod {
+public class FeedbackAddActivity extends BaseLoadActivity implements IFeedbackAddContract.IView {
     @BindView(R.id.edt_content)
     EditText mEdtContent;
     @BindView(R.id.txt_left_number)
     TextView mTxtLeftNumber;
-    @BindView(R.id.upload_img)
-    ImgUploadBlock mUpload;
+    @BindView(R.id.afa_upload_group)
+    ImageUploadGroup mUploadGroup;
     @BindView(R.id.txt_submit)
     TextView mTxtSubmit;
-    @BindView(R.id.ll_scroll_photo)
-    LinearLayout mLlScrollPhoto;
     @Autowired(name = "parcelable")
     FeedbackDetailResp mDetail;
     @BindView(R.id.title_bar)
@@ -77,24 +71,8 @@ public class FeedbackAddActivity extends BaseLoadActivity implements IFeedbackAd
         if (isEditModal()) {//编辑模式
             mTitleBar.setHeaderTitle("继续意见反馈");
         }
-        mEdtContent.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mTxtSubmit.setEnabled(mEdtContent.getText().toString().length() > 0);
-            }
-        });
         mTxtSubmit.setOnClickListener(v -> {
-            mPresent.addFeedback(mEdtContent.getText().toString(), getImageUrls());
+            mPresent.addFeedback(mEdtContent.getText().toString(), mUploadGroup.getUploadImgUrls());
         });
 
     }
@@ -103,39 +81,9 @@ public class FeedbackAddActivity extends BaseLoadActivity implements IFeedbackAd
         return mDetail != null;
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case ImgUploadBlock.REQUEST_CODE_CHOOSE:
-                if (resultCode == RESULT_OK) {
-                    uploadImg(data, this);
-                }
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    @Override
-    public LinearLayout getImageContainer() {
-        return mLlScrollPhoto;
-    }
-
-    @Override
-    public Activity getImageActivity() {
-        return this;
-    }
-
-    @Override
-    public ImgUploadBlock getUploadBlock() {
-        return mUpload;
-    }
-
-    @Override
-    public int getMaxImageNum() {
-        return 5;
+        mUploadGroup.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -154,5 +102,11 @@ public class FeedbackAddActivity extends BaseLoadActivity implements IFeedbackAd
     @Override
     public String getFeedbackID() {
         return isEditModal() ? mDetail.getFeedbackID() : "";
+    }
+
+    @OnTextChanged(R.id.edt_content)
+    void onTextChanged(CharSequence s) {
+        mTxtLeftNumber.setText(String.valueOf(300 - s.length()));
+        mTxtSubmit.setEnabled(s.length() > 0);
     }
 }
