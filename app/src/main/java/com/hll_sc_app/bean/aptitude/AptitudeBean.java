5bff192c32f9a27e4829c15fbc04152c1f1dd830
xuezhixin <xuezhixin@hualala.com>
2020-11-27 12:07:03 +0800

    del 资质类的克隆实现

--- app/src/main/java/com/hll_sc_app/bean/aptitude/AptitudeBean.java
+++ app/src/main/java/com/hll_sc_app/bean/aptitude/AptitudeBean.java
@@ -1,46 +1,17 @@
 package com.hll_sc_app.bean.aptitude;
 
-import com.hll_sc_app.base.utils.UIUtils;
-
-import java.util.Arrays;
-
 /**
  * @author <a href="mailto:xuezhixin@hualala.com">Vixb</a>
  * @since 2020/6/28
  */
 
-public class AptitudeBean implements Cloneable {
+public class AptitudeBean {
     private String aptitudeName;
     private String aptitudeType;
     private String aptitudeUrl;
     private String endTime;
     private String extGroupID;
     private String productInfo;
-
-    @Override
-    public Object clone() {
-        try {
-            return super.clone();
-        } catch (CloneNotSupportedException e) {
-            return this;
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AptitudeBean that = (AptitudeBean) o;
-        return UIUtils.equals(aptitudeName, that.aptitudeName) &&
-                UIUtils.equals(aptitudeType, that.aptitudeType) &&
-                UIUtils.equals(aptitudeUrl, that.aptitudeUrl) &&
-                UIUtils.equals(endTime, that.endTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(new Object[]{aptitudeName, aptitudeType, aptitudeUrl, endTime});
-    }
 
     public String getAptitudeName() {
         return aptitudeName;
