3ad7d9bd308e10b801933be10bc4986011683a6d
xuezhixin <xuezhixin@hualala.com>
2021-03-06 00:23:35 +0800

    update 闪屏页适配底栏

--- base/src/main/java/com/hll_sc_app/base/utils/StatusBarUtil.java
+++ base/src/main/java/com/hll_sc_app/base/utils/StatusBarUtil.java
@@ -31,6 +31,7 @@ public class StatusBarUtil {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
